9638765bce8055d2376021d45c6c79f5e25f103c
Daniel Deutsch <deudan1010gmail.com>
2016-11-20 12:18:05 +0100

    Using states

--- src/app/index.js
+++ src/app/index.js
@@ -3,18 +3,27 @@ var ReactDOM = require("react-dom");
 
 //Create component with react class method
 var TodoComponent = React.createClass({
+  getInitialState:function(){
+    return{
+      todos:["training", "learning", "working"]
+    }
+  },
+
   render:function(){
     return(
-      <div>
-        <p><strong>Wine name: </strong>{this.props.wine.name}</p>
-        <p><strong>Wine smell factor: </strong>{this.props.wine.smellFactor}</p>
-        <p><strong>Wine price: </strong>{this.props.wine.price}</p>
+      <div id="todo-list">
+        <p>Busiest people work hard</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
       </div>
     );//grab this component and put a reference to the render method
-  }
+  }//use the render method to output data
+
 });//create a TodoComponent
 
-var myWine={name: "Mount Blanc", smellFactor:"very flowery", price: "99"};
 
 //Put component into html page
-ReactDOM.render(<TodoComponent mssg="I like wine" wine={myWine}/>, document.getElementById("todo-wrapper"));
+ReactDOM.render(<TodoComponent/>, document.getElementById("todo-wrapper"));
