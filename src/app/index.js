9aaedbf02482f55f15f3b0826a9ecb92df8857bd
Daniel Deutsch <deudan1010gmail.com>
2016-11-20 13:37:16 +0100

    handling events

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-//Create component with react class method
+//Create components with react class method
 var TodoComponent = React.createClass({
   getInitialState:function(){
     return{
@@ -10,13 +10,12 @@ var TodoComponent = React.createClass({
   },
 
   render:function(){
-
     var todos=this.state.todos;//local version of the state variable
     todos = todos.map(function(item,index){
       return(
-        <TodoItem item={item} key={index}/>
+        <TodoItem item={item} key={index} onDelete={this.onDelete}/>
       );
-    });
+    }.bind(this));
 
     return(
       <div id="todo-list">
@@ -25,8 +24,17 @@ var TodoComponent = React.createClass({
         <ul>{todos}</ul>
       </div>
     );//grab this component and put a reference to the render method
-  }//use the render method to output data
+  },//use the render method to output data
 
+  //custom functions
+  onDelete:function(item){
+    var updatedTodos=this.state.todos.filter(function(val, index){
+      return item !== val;
+    });//create a new array with the filter method
+    this.setState({
+      todos:updatedTodos
+    });//set a new State with the new array
+  }//onDelete function
 });//create a TodoComponent
 
 var TodoItem=React.createClass({
@@ -34,12 +42,18 @@ var TodoItem=React.createClass({
     return(
       <li>
         <div className="todo-item">
-          <span className="item.name">{this.props.item}</span>
+          <span className="item-name">{this.props.item} </span>
+          <span className="item-delete" onClick={this.handleDelete}> X</span>
         </div>
       </li>
     );
+  },//render
+
+  //custom functions
+  handleDelete:function(){
+    this.props.onDelete(this.props.item);
   }
-});
+});//create a TodoItemComponent
 
 
 //Put component into html page
