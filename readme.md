93f88d5fc884cf544b4a465cfe800c2dbb7df726
Daniel Deutsch <deudan1010gmail.com>
2016-11-20 11:48:38 +0100

    buidling first working component

--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 React.js Tutorial - ToDo List
 ===
-Written by Daniel Deutsch
----
-Tutorial by Youtube Net Ninja
----
+##Written by Daniel Deutsch
+
+###Tutorial by Youtube Net Ninja
+
 Intro for **React.js**
+
+*Stay motivated!*
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-alert("It works!");
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//Create component with react class method
+var TodoComponent = React.createClass({
+  render:function(){
+    return(
+      <div>
+        <h1>Helooo</h1>
+        <p>Test</p>
+      </div>
+    );
+  }
+});
+
+//Put component into html page
+ReactDOM.render(<TodoComponent/>, document.getElementById("todo-wrapper"));
